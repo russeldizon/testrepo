a2fc7f7f993e9f26fbfc3a7de6cb640ba511d10b
russeldizon <rldizon@convergeict.com>
2017-03-29 18:57:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,14 +7,15 @@ PERSONS = [
     {"NAME": "JOANNE", "AGE": 18, "LOCATION": "PARANAQUE"},
 ]
 
-r =[]
-u = []
+
+
 
 def filter_by_age_range(age_min, age_max):
     # Returns the list of NAMEs within the provided age range
     # using PERSONS as data table
     # :age_min: minimum limit of the range
     # :age_max: maximum limit of the range
+    r =[]
     for i in range(0,len(PERSONS)):
 
       if age_min <= PERSONS [i]["AGE"] <= age_max:
@@ -28,18 +29,20 @@ def filter_by_location(location):
     # Returns the list of NAMEs from the given using PERSONS as data table
     # location
     # :location: place
+    l = []
     for i in range(0,len(PERSONS)):
 
         if location == PERSONS[i]["LOCATION"]:
-            return PERSONS[i]["NAME"]
-
+            l.append(PERSONS[i]["NAME"])
+    
+    return l
     pass
 
 
 def sorted_name():
     # Returns the list of NAMEs in ascending order (A-Z) using PERSONS
     # as data table
-
+    u = []
     for i in range(0,len(PERSONS)):
 
         u.append(PERSONS[i]["NAME"])
